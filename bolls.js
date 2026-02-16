b6d134a9a1d71a01eb27d17007c5fc9eea6460ef
Ahmad Spierij <ahmadspierij@gmail.com>
2026-02-15 23:29:11 -0500

    solved set partion problem in javaScript

--- bolls.js
+++ bolls.js
@@ -12,22 +12,101 @@ let nums = [1, 1, 4, -6, 8, 5, 9, 1, 2, 9, -1];
 
 // console.log(suffixes);
 
-const highestCombinationOfTwo = (list) => {
-  let highest = 0;
-  for (let i = 0; i < list.length; ++i) {
-    if (i < list.length - 1) {
-      let local =
-        list[i] > list[i] + list[i + 1] ? list[i] : list[i] + list[i + 1];
-      highest = local > highest ? local : highest;
-    } else {
-      highest = list[i] > highest ? list[i] : highest;
-    }
+// const highestCombinationOfTwo = (list) => {
+//   let highest = 0;
+//   for (let i = 0; i < list.length; ++i) {
+//     if (i < list.length - 1) {
+//       let local =
+//         list[i] > list[i] + list[i + 1] ? list[i] : list[i] + list[i + 1];
+//       highest = local > highest ? local : highest;
+//     } else {
+//       highest = list[i] > highest ? list[i] : highest;
+//     }
+//   }
+//   return highest;
+// };
+const highestCombinationOfTwo = (list, highest = []) => {
+  // console.log(list.length);
+  if (list.length === 0) {
+    return;
   }
+  if (list.length === 1) {
+    highest.push(list[0]);
+    // console.log("I'am here 1");
+    return;
+  }
+  if (list.length === 2) {
+    // console.log("I'am here 2");
+    let local = list[0] > list[0] + list[1] ? list[0] : list[0] + list[1];
+    highest.push(local);
+    return highest;
+  }
+  for (let i = list.length; i > 2; --i) {
+    highestCombinationOfTwo(list.slice(i), highest);
+  }
+  // highestCombinationOfTwo(list.slice(1), highest);
+  // console.log(highest);
   return highest;
 };
 
-console.log(highestCombinationOfTwo(nums));
+// highestCombinationOfTwo(nums);
+// console.log(highestCombinationOfTwo(nums));
+
+/*
+Python implemention
+# From codereview.stackexchange.com
+def partitions(set_):
+    if not set_:
+        yield []
+        return
+    for i in range(2**len(set_)//2):  # skip duplicates How?
+        parts = [set(), set()]
+        for item in set_:
+            parts[i & 1].add(item)
+            i >>= 1
+        for b in partitions(parts[1]):
+            yield [parts[0]]+b
 
 
+def get_partitions(set_):
+    for partition in partitions(set_):
+        yield [list(elt) for elt in partition]
 
+*/
 
+function* partitions(inputSet) {
+  const elements = Array.from(inputSet);
+  const n = elements.length;
+  if (n === 0) {
+    yield [];
+    return;
+  }
+
+  const limit = 2 ** n / 2;
+
+  for (let i = 0; i < limit; ++i) {
+    let itemI = i;
+    let parts = [new Set(), new Set()];
+    for (const item of elements) {
+      parts[itemI & 1].add(item);
+      itemI <<= 1;
+    }
+
+    for (const b of partitions(parts[1])) {
+      yield [parts[0], ...b];
+    }
+  }
+}
+
+function* getPartitions(inputSet) {
+  for (const partition of partitions(inputSet)) {
+    yield partition.map((s) => Array.from(s));
+  }
+}
+
+//
+const mySet = [1, 2, 3];
+console.log(`Partitions for [${mySet}]:`);
+for (const p of getPartitions(mySet)) {
+  console.log(JSON.stringify(p));
+}
